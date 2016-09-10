10d43d6e0d86ea123faf495a583b08325201bb88
b8bauer <bbauer@cca.edu>
2016-09-09 17:12:51 -0700

    Create puzzle_09.js

---
+++ puzzle_09.js
@@ -0,0 +1,16 @@
+for (var steps = 0; steps < 9; steps++){
+down();
+}
+right();
+  for (var stairs = 0; stairs < 8; stairs++){
+    setColor("red");
+	  right();
+    setColor("blue");
+    right();
+    setColor("green");
+    right();
+    setColor("yellow");
+    right();
+  	up();
+  }
+up();
